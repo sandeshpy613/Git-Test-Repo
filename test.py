68c37556271e520ca2c7fe2ea58c8c8d2fb8726a
sandeshpy613 <sandeshpy613@gmail.com>
2025-05-23 15:21:17 +0530

    2nd Commit

--- test.py
+++ test.py
@@ -5,3 +5,8 @@ is_active = True # Boolean
 
 print(f"Hello, {name}!")
 print(f"Age: {age}, Height: {height}, Active: {is_active}")
+
+# Extra things added to Branch1
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
